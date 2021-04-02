ac3c2cbb4a59a244cfdcdac0550ef3e2fa77c775
Chris M <cmiyachi@alum.mit.edu>
2021-04-01 21:10:35 -0400

    Final working version

--- src/main/java/blog/client/BlogClient.java
+++ src/main/java/blog/client/BlogClient.java
@@ -1,5 +1,6 @@
 package blog.client;
 
+import com.proto.blog.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -13,6 +14,76 @@ public class BlogClient {
     }
 
     private void run() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
 
+        BlogServiceGrpc.BlogServiceBlockingStub blogClient = BlogServiceGrpc.newBlockingStub(channel);
+
+        Blog blog = Blog.newBuilder()
+                .setAuthorId("Christine")
+                .setTitle("New blog!")
+                .setContent("Hello world this is my first blog!")
+                .build();
+
+        CreateBlogResponse createResponse = blogClient.createBlog(
+                CreateBlogRequest.newBuilder()
+                        .setBlog(blog)
+                        .build()
+        );
+
+        System.out.println("Received create blog response");
+        System.out.println(createResponse.toString());
+
+        String blogId = createResponse.getBlog().getId();
+
+        System.out.println("Reading blog....");
+
+        ReadBlogResponse readBlogResponse = blogClient.readBlog(ReadBlogRequest.newBuilder()
+                .setBlogId(blogId)
+                .build());
+
+        System.out.println(readBlogResponse.toString());
+        // trigger a not found error
+//        System.out.println("Reading blog with non existing id....");
+//
+//        ReadBlogResponse readBlogResponseNotFound = blogClient.readBlog(ReadBlogRequest.newBuilder()
+//                .setBlogId("5bd83b5cf476074e2bff3495")
+//                .build());
+
+
+        Blog newBlog = Blog.newBuilder()
+                .setId(blogId)
+                .setAuthorId("Changed Author")
+                .setTitle("New blog (updated)!")
+                .setContent("Hello world this is my first blog! I've added some more content")
+                .build();
+
+        System.out.println("Updating blog...");
+        UpdateBlogResponse updateBlogResponse = blogClient.updateBlog(
+                UpdateBlogRequest.newBuilder().setBlog(newBlog).build());
+
+        System.out.println("Updated blog");
+        System.out.println(updateBlogResponse.toString());
+
+        System.out.println("Deleting blog");
+        DeleteBlogResponse deleteBlogResponse = blogClient.deleteBlog(
+                DeleteBlogRequest.newBuilder().setBlogId(blogId).build()
+        );
+
+        System.out.println("Deleted blog");
+
+
+//        System.out.println("Reading blog....");
+////        // this one should return NOT_FOUND
+//        ReadBlogResponse readBlogResponseAfterDeletion = blogClient.readBlog(ReadBlogRequest.newBuilder()
+//                .setBlogId(blogId)
+//                .build());
+
+        // we list the blogs in our database
+        // we list the blogs in our database
+        blogClient.listBlog(ListBlogRequest.newBuilder().build()).forEachRemaining(
+                listBlogResponse -> System.out.println(listBlogResponse.getBlog().toString())
+        );
     }
 }
--- src/main/java/blog/server/BlogServer.java
+++ src/main/java/blog/server/BlogServer.java
@@ -1,15 +1,16 @@
 package blog.server;
 
-import com.github.cmiyachi.calculator.server.CalculatorServerImpl;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
+import io.grpc.protobuf.services.ProtoReflectionService;
 import java.io.IOException;
 
 public class BlogServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server = ServerBuilder.forPort(50051)
                 .addService(new BlogServerImpl())
+                .addService(ProtoReflectionService.newInstance()) // reflection
                 .build();
 
         server.start();
